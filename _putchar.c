f3b3d660539e06c64c332b1e76394f92e01ed17a
Fatima20027 <fatima.hailou@icloud.com>
2024-01-26 14:08:48 +0100

    added to putchar

--- _putchar.c
+++ _putchar.c
@@ -1,6 +1,14 @@
 #include "main.h"
 
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to be written
+ *
+ * Return: 1. success. -1 is returned error.
+ */
+
 void _putchar(char c)
 {
-	write (1, &c, 1);
+	write(1, &c, 1);
 }
+
