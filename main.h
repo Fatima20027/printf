e9fa6cc7f0c8bec2386b1c57418dd31978dce9cc
Fatima20027 <fatima.hailou@icloud.com>
2024-01-26 15:18:19 +0100

    printchr function protitype added and some liberies included

--- main.h
+++ main.h
@@ -3,10 +3,17 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <assert.h>
 
 void _putchar(char c);
 int _printf(const char *format, ...);
 int allspechandler(char specifier, va_list hoho);
+int printchr(int c);
 
 #endif
 
