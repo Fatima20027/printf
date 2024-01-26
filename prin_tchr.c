ac8766415ea0b892d5202d6ea9b1e34e5bcc1a3e
MLankaoui <marouanelankaoui66@gmail.com>
2024-01-26 15:59:03 +0100

    fixed betty on print_chr.c

--- prin_tchr.c
+++ prin_tchr.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+* printchr - Prints a character into the default output
+* @c: The character to be printed
+*
+* Return: Returns the number of characters printed upon success (1).
+* Returns -1 if not.
+*/
 int printchr(int c)
 {
 	return (write(1, &c, 1));
