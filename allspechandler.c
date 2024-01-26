68fd87a4c1deb9565d13a85903cda4fbb0cc2ef3
Fatima20027 <fatima.hailou@icloud.com>
2024-01-26 14:58:51 +0100

    fixing betty and adding description for the function

--- allspechandler.c
+++ allspechandler.c
@@ -1,9 +1,17 @@
 #include "main.h"
 
+/**
+ * allspechandler - Handles a specific specifier
+ * @specifier: The specifier character
+ * @hoho: The va_list containing the argument
+ *
+ * Return: The number of characters processed
+ */
+
 int allspechandler(char specifier, va_list hoho)
 {
 	int characterCtr = 0;
 
-	if (spec = 'c')
-		characterCtr += printcharacter(va_arg(hoho), int); 
+	if (spec == 'c')
+		characterCtr += printcharacter(va_arg(hoho), int);
 }
