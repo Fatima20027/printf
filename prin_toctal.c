d726353e934b402c1aabae550cdbb37a42b61b55
MLankaoui <marouanelankaoui66@gmail.com>
2024-01-27 14:42:03 +0100

    octal feature added to printf , and logic implemented in printoct function

---
+++ prin_toctal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int printoct(unsigned int number)
+{
+	int characterCtr = 0;
+
+	char octaldigit;
+
+	if (number / 8 != 0)
+		characterCtr += printoct(number / 8);
+
+	octaldigit = '0' + number % 8;
+
+	characterCtr += write(1, &octaldigit, 1);
+
+	return (characterCtr);
+}
