6d41fe48d4a502b785ab0152732a6465859d433c
Fatima20027 <fatima.hailou@icloud.com>
2024-01-30 13:45:52 +0100

    prin_tunsigned.c was updated

--- prin_tunsigned.c
+++ prin_tunsigned.c
@@ -9,16 +9,16 @@
 
 int printun(unsigned int number)
 {
-	int characterCtr = 0;
+	int charactCt = 0;
 
 	char digit;
 
 	if (number / 10 != 0)
-		characterCtr += printun(number / 10);
+		charactCt += printun(number / 10);
 
 	digit = '0' + number % 10;
 
-	characterCtr += write(1, &digit, 1);
+	charactCt += write(1, &digit, 1);
 
-	return (characterCtr);
+	return (charactCt);
 }
